6408df9ba0ed63e940c5651f90f817c15d78677d
Guilherme Pacheco <guilherfp@gmail.com>
2016-03-09 20:31:57 -0300

    Signed-off-by: Guilherme Pacheco <guilherfp@gmail.com>

--- src/main/java/br/com/devsource/teste/controller/UserController.java
+++ src/main/java/br/com/devsource/teste/controller/UserController.java
@@ -1,15 +1,10 @@
 package br.com.devsource.teste.controller;
 
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import br.com.devsource.teste.user.User;
 import br.com.devsource.teste.user.UserRepository;
 
 /**
@@ -19,16 +14,12 @@ import br.com.devsource.teste.user.UserRepository;
 @RequestMapping("/users")
 public class UserController {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
-
   @Autowired
   private UserRepository userRepository;
 
   @RequestMapping({ "", "/" })
   public String users(Model model) {
-    List<User> allUsers = userRepository.listAll();
-    LOGGER.info("Users: {}", allUsers);
-    model.addAttribute("users", allUsers);
+    model.addAttribute("users", userRepository.listAll());
     return "users";
   }
 }
