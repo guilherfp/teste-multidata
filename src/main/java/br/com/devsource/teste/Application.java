f5708b76c57084afbc9e0157e000bf560a8898d0
Guilherme Pacheco <guilherfp@gmail.com>
2016-03-09 18:51:32 -0300

    Signed-off-by: Guilherme Pacheco <guilherfp@gmail.com>

--- src/main/java/br/com/devsource/teste/Application.java
+++ src/main/java/br/com/devsource/teste/Application.java
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Guilherme Pacheco
  */
 @SpringBootApplication
-public class Main {
+public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(Main.class, args);
+    SpringApplication.run(Application.class, args);
   }
 
 }
