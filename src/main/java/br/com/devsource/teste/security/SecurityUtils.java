ec2a4d59a8cc195530c09b370fb7a3e6488a6f57
Guilherme Pacheco <guilherfp@gmail.com>
2016-03-09 21:04:35 -0300

    Signed-off-by: Guilherme Pacheco <guilherfp@gmail.com>

--- src/main/java/br/com/devsource/teste/security/SecurityUtils.java
+++ src/main/java/br/com/devsource/teste/security/SecurityUtils.java
@@ -1,46 +1,35 @@
 package br.com.devsource.teste.security;
 
-import java.util.function.Predicate;
-
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.security.authentication.AuthenticationServiceException;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 /**
  * @author Guilherme Pacheco
  */
 final class SecurityUtils {
 
-  private static final String ANONYMOUS = "ROLE_ANONYMOUS";
-
   private SecurityUtils() {
     super();
   }
 
-  public static boolean isAuthenticated(Authentication auth) {
-    return auth == null ? false : auth.getAuthorities().stream().anyMatch(noAnonymous());
-  }
-
-  private static Predicate<GrantedAuthority> noAnonymous() {
-    return a -> !a.getAuthority().equalsIgnoreCase(ANONYMOUS);
-  }
-
   public static void notNull(Object value, String message) {
     if (value == null) {
-      throw new AuthenticationServiceException(message);
+      throw new AuthenticationCredentialsNotFoundException(message);
     }
   }
 
   public static void isTrue(boolean value, String message) {
     if (!value) {
-      throw new AuthenticationServiceException(message);
+      throw new LockedException(message);
     }
   }
 
   public static void notBlank(String value, String message) {
     if (StringUtils.isBlank(value)) {
-      throw new AuthenticationServiceException(message);
+      throw new UsernameNotFoundException(message);
     }
   }
+
 }
